db580e2dbdbddc41bc422b1c03102a5500835792
Максим <maksim.ardintsev@mail.ru>
2024-04-22 20:59:40 +0300

    Раскомментирован код

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,49 +6,49 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-        //    #region out int console
-        //    Console.WriteLine("Введите ваше имя: ");
-        //    string name = Console.ReadLine();
+            #region out int console
+            Console.WriteLine("Введите ваше имя: ");
+            string name = Console.ReadLine();
 
-        //    Console.WriteLine("Введите ваш возраст: ");
-        //    int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите ваш возраст: ");
+            int age = int.Parse(Console.ReadLine());
 
-        //    Console.WriteLine("Введите ваш город: ");
-        //    string city = Console.ReadLine();
+            Console.WriteLine("Введите ваш город: ");
+            string city = Console.ReadLine();
 
-        //    Console.WriteLine("Введите ваш рост: ");
-        //    double height = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите ваш рост: ");
+            double height = double.Parse(Console.ReadLine());
 
-        //    // Конкатенация 
-        //    Console.WriteLine("Ваше имя: " + name);
-        //    Console.WriteLine("Ваш возраст: " + age);
-        //    Console.WriteLine("Ваш город: " + city);
-        //    Console.WriteLine("Ваш рост: " + height);
+            // Конкатенация 
+            Console.WriteLine("Ваше имя: " + name);
+            Console.WriteLine("Ваш возраст: " + age);
+            Console.WriteLine("Ваш город: " + city);
+            Console.WriteLine("Ваш рост: " + height);
 
-        //    // Плейсхолдеры (placeholder)
-        //    Console.WriteLine("Ваше имя: {0}, ваш возраст: {1}, ваш город: {2}, ваш рост: {3}",name, age, city, height);
+            // Плейсхолдеры (placeholder)
+            Console.WriteLine("Ваше имя: {0}, ваш возраст: {1}, ваш город: {2}, ваш рост: {3}", name, age, city, height);
 
-        //    // Интерполяция строк
-        //    Console.WriteLine($"Ваше имя: {name}, ваш возраст: {age}, ваш город: {city}, ваш рост: {height}");
+            // Интерполяция строк
+            Console.WriteLine($"Ваше имя: {name}, ваш возраст: {age}, ваш город: {city}, ваш рост: {height}");
 
-        //    #endregion
+            #endregion
 
-        //    #region MyMethod
-        //    Print("Это метод Print");
-        //    #endregion
+            #region MyMethod
+            Print("Это метод Print");
+            #endregion
 
-        //    #region MyMethod2
-        //    int a = Addition(4, 20);
-        //    #endregion
+            #region MyMethod2
+            int a = Addition(4, 20);
+            #endregion
 
-        //    #region object of Client
-        //    Client client = new Client();
-        //    client.Name = "Maksim";
-        //    client.OrderNumber = 51451351;
-        //    client.ID = 1;
+            #region object of Client
+            Client client = new Client();
+            client.Name = "Maksim";
+            client.OrderNumber = 51451351;
+            client.ID = 1;
 
-        //    Console.WriteLine(client);
-        //    #endregion
+            Console.WriteLine(client);
+            #endregion
 
             #region Fraction
             Fraction fraction1 = new Fraction(1,2);
